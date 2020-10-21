3e9174e78ad10c2cee608d5297ec406e800e2717
Ivan Manov <ivanmanov@live.com>
2020-10-21 14:52:17 +0300

    Min swift to 5.3. Package resources

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,7 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Sqaner",
-            dependencies: []),
+            resources: [
+                .process("Sqaner.storyboard"),
+                .process("Media.xcassets")
+            ]),
         .testTarget(
             name: "SqanerTests",
             dependencies: ["Sqaner"])
